7d77b7aafdabc9208a0814d75270a12e65e0a988
Krasnopolskiy <i1.krasnopolskiy@gmail.com>
2020-09-28 21:27:07 +0300

    Fix api/note/create re #issue#1

--- codeshare/api/urls.py
+++ codeshare/api/urls.py
@@ -4,10 +4,12 @@ from . import views
 urlpatterns = [
     path('welcome/', views.welcome),
     path('note/get/<str:note_name>', views.note_get),
+
     path('note/create', views.note_create),
     path('note/update', views.welcome),
     path('note/publish', views.welcome),
     path('note/delete', views.welcome),
+
     path('note/list', views.note_list),     # remove it
     path('author/list', views.author_list)  # remove it
 ]
