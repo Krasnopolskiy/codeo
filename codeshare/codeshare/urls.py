1fc160dfb920bb0e90f65e4482ca2514c600ec64
GigaNigga <i1.krasnolskiy@gmail.com>
2020-10-02 11:00:56 +0300

    Merged urls.py re #issue#3

--- codeshare/codeshare/urls.py
+++ codeshare/codeshare/urls.py
@@ -6,10 +6,6 @@ urlpatterns = [
     path('signup/', include('signup.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-<<<<<<< HEAD
     path('', include('notes.urls')),
     path('',include('django.contrib.auth.urls'))
-=======
-    path('', include('notes.urls'))
->>>>>>> dev
 ]
