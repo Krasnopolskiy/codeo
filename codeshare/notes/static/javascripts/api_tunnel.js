7dd8e16b413b2f3491913313bf23465b274aea4c
GigaNigga <i1.krasnolskiy@gmail.com>
2020-10-02 13:53:36 +0300

    Added published and protected checkboxes re #issue#4

--- codeshare/notes/static/javascripts/api_tunnel.js
+++ codeshare/notes/static/javascripts/api_tunnel.js
@@ -1,6 +1,7 @@
 class Api_tunnel {
-    constructor(csrftoken) {
+    constructor(csrftoken, notename) {
         this.csrftoken = csrftoken
+        this.notename = notename
     }
 
     create = async (body = {}) => {
@@ -15,22 +16,25 @@ class Api_tunnel {
         return await res.json()
     }
 
-    retrieve = async (notename) => {
-        let res = await fetch('/api/note/retrieve/' + notename, {
+    retrieve = async () => {
+        let res = await fetch('/api/note/retrieve/' + this.notename, {
             "method": "GET",
             "headers": { 'X-CSRFToken': this.csrftoken }
         })
         return await res.json()
     }
 
-    update = async (notename, source, onclose = false) => {
+    update = async (body, onclose = false) => {
+        console.log(JSON.stringify(Object.assign({
+            "name": this.notename,
+            "onclose": onclose
+        }, body)))
         let res = await fetch('/api/note/update', {
             method: 'PUT',
-            body: JSON.stringify({
-                "name": notename,
-                "source": source,
+            body: JSON.stringify(Object.assign({
+                "name": this.notename,
                 "onclose": onclose
-            }),
+            }, body)),
             headers: {
                 'Content-Type': 'application/json',
                 'X-CSRFToken': this.csrftoken
--- codeshare/notes/static/javascripts/editor.js
+++ codeshare/notes/static/javascripts/editor.js
@@ -1,21 +1,39 @@
 const url = new URL(window.location.href)
 const notename = url.pathname.split('/')[1]
-const api = new Api_tunnel(Cookies.get('csrftoken'))
+const api = new Api_tunnel(Cookies.get('csrftoken'), notename)
 const editor = ace.edit("editor")
 
 
 editor.session.setMode("ace/mode/plain_text")
 editor.setTheme("ace/theme/monokai")
 editor.session.setUseSoftTabs(false)
+editor.setShowPrintMargin(false)
 editor.session.setTabSize(4)
 editor.setReadOnly(true)
+editor.setFontSize(18)
 
 
 let error = false
+let update = () => {
+    if (!editor.getReadOnly())
+        api.update({
+            "source": editor.getValue(),
+            "language": editor.session.getMode().$id,
+            "published": $('#note_published')[0].checked,
+            "protected": $('#note_protected')[0].checked
+        })
+            .then(data => {
+                console.log(data)
+                if (data["message"] == "error") {
+                    error = true
+                    location = url.origin
+                }
+            })
+}
 
 
 if (notename.length > 0) {
-    api.retrieve(notename)
+    api.retrieve()
         .then(data => {
             if (data["message"] == "retrieved") {
                 editor.setValue(data.source)
@@ -51,21 +69,19 @@ $('#editor').click(() => {
     }
 })
 
-$('#editor').keyup(() => {
-    if (!editor.getReadOnly())
-        api.update(notename, editor.getValue())
-            .then(data => {
-                console.log(data)
-                if (data["message"] == "error") {
-                    error = true
-                    location = url.origin
-                }
-            })
+$('#editor').keyup(() => { update() })
+$('#note_protected_btn').click(() => {
+    $('#note_protected')[0].checked = !$('#note_protected')[0].checked
+    update()
+})
+$('#note_published_btn').click(() => {
+    $('#note_published')[0].checked = !$('#note_published')[0].checked
+    update()
 })
 
 $(window).on("beforeunload", () => {
     if (api.ismine && !editor.getReadOnly() && !error)
-        api.update(notename, editor.getValue(), true)
+        api.update({ "source": editor.getValue() }, true)
             .then(data => {
                 console.log(data)
             })
