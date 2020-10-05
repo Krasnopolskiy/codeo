3ad2457616df651e39070f239bf96a8aeaee254a
bykavki <minh.1240.1240@gmail.com>
2020-10-05 15:28:33 +0300

    Registration

--- codeshare/signup/views.py
+++ codeshare/signup/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 
 def signup(response):
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/home")
+    else:
+        form = UserCreationForm
     form = UserCreationForm()
     return render(response, "registration/signup.html", {"form":form})
