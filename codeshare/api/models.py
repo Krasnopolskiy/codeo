6dba76c73b21fedfbe6d9375860498a72e0d9273
bykavki <minh.1240.1240@gmail.com>
2020-10-05 18:45:40 +0300

    Update models.py

--- codeshare/api/models.py
+++ codeshare/api/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Author(models.Model):
+    user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
     uid = models.CharField(max_length=16)
-
     def __str__(self):
         return self.uid
 
@@ -12,7 +12,7 @@ class Note(models.Model):
     name = models.CharField(max_length=4)
 
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    language = models.CharField(max_length=20)
+    language = models.CharField(max_length=20, default ='')
     published = models.BooleanField(default=False)
     protected = models.BooleanField(default=False)
     collaborator_link = models.CharField(max_length=6, blank=True)
