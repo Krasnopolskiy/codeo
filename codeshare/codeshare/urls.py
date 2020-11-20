da3cfac79c977e890910c3b770e6921214b58315
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2020-11-20 12:12:08 +0300

    Fix routing re #dev

--- codeshare/codeshare/urls.py
+++ codeshare/codeshare/urls.py
@@ -1,22 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
-from notes import views as v
-from django.conf.urls import url
-from django.conf import settings
+from notes import views
 from django.contrib.auth import logout
 
 
-
 urlpatterns = [
-    #url(r'^logout/$', logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
-    #url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='user_logout'),
-    path('login/', v.login_page, name = 'login'),
-    path('logout/', v.logout_view, name = 'logout'),
-    path('signup/', v.signup, name='signup'),
+    path('user/signup/', views.signup, name='signup'),
+    path('user/login/', views.login_page, name='login'),
+    path('user/home/', views.userPage, name='user page'),
+    path('user/logout/', views.logout_view, name='logout'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('notes.urls')),
-    path('', include('django.contrib.auth.urls')),
-    path('userpage/', v.userPage, name='user page'),
-
+    path('', include('notes.urls'))
 ]
--- codeshare/notes/urls.py
+++ codeshare/notes/urls.py
@@ -1,8 +1,8 @@
-from django.urls import include, path, re_path
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'^(?P<name>\w{4,6})/$', views.index, name='index')
+    path('<str:name>', views.index, name='index')
 ]
