0d9af6e6cbab3f8225c171de98fb40168f610215
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-01-15 13:13:19 +0300

    Add delete button

--- notes/static/js/editor.js
+++ notes/static/js/editor.js
@@ -11,6 +11,7 @@ if (!error)
     ismine = init_data['ismine']
     read_link = url.host + '/' + init_data['read_link']
     edit_link = url.host + '/' + init_data['edit_link']
+    $('#delete-btn').attr('href', 'delete/' + init_data['edit_link'] + '/')
     $('#name-settings .input-group input').val(note['name'])
     $('#read-settings .input-group input').val(read_link)
     $('#edit-settings .input-group input').val(edit_link)
