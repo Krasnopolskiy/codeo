740f7e8e45ae12bc4722ab0b36e74a43eab6cb12
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-01-22 01:00:30 +0300

    Update icons

--- notes/forms.py
+++ notes/forms.py
@@ -19,12 +19,12 @@ class LoginForm(forms.Form):
         self.helper.form_show_labels = False
         self.helper.layout = Layout(
             Div(
-                HTML('<span class="input-group-text fas fa-user"></span>'),
+                HTML('<span class="input-group-text bi fas fa-user"></span>'),
                 HTML('<input class="form-control" name="username" placeholder="Username">'),
                 css_class='input-group mt-4'
             ),
             Div(
-                HTML('<span class="input-group-text fas fa-lock"></span>'),
+                HTML('<span class="input-group-text bi fas fa-lock"></span>'),
                 HTML('<input class="form-control" type="password" name="password" placeholder="Password">'),
                 css_class='input-group mt-3'
             ),
@@ -46,22 +46,22 @@ class SignupForm(forms.Form, RegistrationForm):
         self.helper.form_show_labels = False
         self.helper.layout = Layout(
             Div(
-                HTML('<span class="input-group-text fas fa-user"></span>'),
+                HTML('<span class="input-group-text bi fas fa-user"></span>'),
                 HTML('<input class="form-control" name="username" placeholder="Username">'),
                 css_class='input-group mt-4'
             ),
             Div(
-                HTML('<span class="input-group-text fas fa-envelope"></span>'),
+                HTML('<span class="input-group-text bi fas fa-envelope"></span>'),
                 HTML('<input class="form-control" type="email" name="email" placeholder="Email">'),
                 css_class='input-group mt-3'
             ),
             Div(
-                HTML('<span class="input-group-text fas fa-lock"></span>'),
+                HTML('<span class="input-group-text bi fas fa-lock"></span>'),
                 HTML('<input class="form-control" type="password" name="password1" placeholder="Password">'),
                 css_class='input-group mt-3'
             ),
             Div(
-                HTML('<span class="input-group-text fas fa-lock"></span>'),
+                HTML('<span class="input-group-text bi fas fa-lock"></span>'),
                 HTML('<input class="form-control" type="password" name="password2" placeholder="Repeat password">'),
                 css_class='input-group mt-3'
             ),
