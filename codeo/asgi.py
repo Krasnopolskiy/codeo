a2603d48fabc93fb20d0995a86ea4660c075e172
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-02-24 23:25:29 +0300

    Change gunicorn to daphe

--- codeo/asgi.py
+++ codeo/asgi.py
@@ -1,12 +1,12 @@
+import django
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import os
-
 from . import urls
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codeo.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
