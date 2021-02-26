21b6e74261bdc7e9dfb5cbd93216b6b61231e847
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-02-26 10:14:11 +0300

    Update registration forms

--- main/forms.py
+++ main/forms.py
@@ -6,16 +6,19 @@ from crispy_forms.layout import Div, HTML, Layout, ButtonHolder, Submit
 
 
 class InputBuilder:
-    def __init__(self, name, type, placeholder):
+    def __init__(self, name, type, placeholder, icon):
         self.placeholder = placeholder
         self.name = name
         self.type = type
+        self.icon = icon
 
     def build(self):
         return Div(
             HTML(f'<input class="form-control" type="{self.type}" name="{self.name}"\
                     id="{self.name}-input" placeholder="{self.placeholder}">'),
-            HTML(f'<label for="{self.name}-input">{self.placeholder}</label>'),
+            HTML(f'<label for="{self.name}-input">\
+                    <span class="bi fas {self.icon} me-2"></span>{self.placeholder}\
+                    </label>'),
             css_class='form-floating text-dark my-4'
         )
 
@@ -25,8 +28,8 @@ class LoginForm(AuthenticationForm):
         super().__init__(request=request, *args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            InputBuilder('username', 'username', 'Username').build(),
-            InputBuilder('password', 'password', 'Password').build(),
+            InputBuilder('username', 'username', 'Username', 'fa-user').build(),
+            InputBuilder('password', 'password', 'Password', 'fa-lock').build(),
             ButtonHolder(Submit('submit', 'Log in'), css_class='mt-3'),
         )
 
@@ -36,9 +39,9 @@ class SignupForm(RegistrationForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            InputBuilder('username', 'username', 'Username').build(),
-            InputBuilder('email', 'email', 'Email address').build(),
-            InputBuilder('password1', 'password', 'Password').build(),
-            InputBuilder('password2', 'password', 'Password confirmation').build(),
+            InputBuilder('username', 'username', 'Username', 'fa-user').build(),
+            InputBuilder('email', 'email', 'Email address', 'fa-envelope').build(),
+            InputBuilder('password1', 'password', 'Password', 'fa-lock').build(),
+            InputBuilder('password2', 'password', 'Password confirmation', 'fa-lock').build(),
             ButtonHolder(Submit('submit', 'Sign up'), css_class='mt-3'),
         )
